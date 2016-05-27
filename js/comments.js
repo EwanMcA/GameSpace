05fb9a671c83116f6d2856b22ae85a858b5b40bb
Elizabeth Walker <eliwalks84@gmail.com>
2016-05-27 21:40:59 +1000

    No Comment

--- js/comments.js
+++ js/comments.js
@@ -12,3 +12,17 @@ $(document).ready(function(){
 		}; 
 	});
 });
+
+setInterval(function(){
+	$.get("getComments.php", function(result){
+		var table = $("#commentBox");
+		var json_result = JSON.parse(result);
+		for(var i in json_result) {
+			var comments = json_result[i];
+			var th = $("<tr></tr>");
+			//th.append($("<td class='time'>" + comments.timestamp + "</td>"));
+			th.append($("<td class='user'>" + comments.userName + "</td>"));
+			th.append($("<td class='comment'>" + comments.comment + "</td>")); 
+			table.append(th);
+		}; 
+	}), 200);
