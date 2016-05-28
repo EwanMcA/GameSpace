6cf926246c252624cae583d703cc5aea5ae01536
Elizabeth Walker <eliwalks84@gmail.com>
2016-05-28 22:22:06 +1000

    Scores for days

--- js/splashScores.js
+++ js/splashScores.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 		console.log(result);
 		var json_result = JSON.parse(result);
 		for(var i in json_result) {
-			var comments = json_result[i]; 
+			var scores = json_result[i]; 
 			var th = $("<tr></tr>");
-			th.append($("<td class='user'>" + comments.userName + "</td>"));
-			th.append($("<td class='score'>" + comments.score + "</td>")); 
+			th.append($("<td class='user'>" + scores.userName + "</td>"));
+			th.append($("<td class='highScore'>" + scores.highScore + "</td>")); 
 			table.append(th);
 		}; 
 	});
