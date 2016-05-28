cfb8900c0aacf85f6ea2e8b025d3db35e162ff49
tigermants <tobiassmithy@gmail.com>
2016-05-28 14:30:27 +1000

    splash game and comments broken

--- splash.php
+++ splash.php
@@ -1,101 +1,270 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE html>
-<html>
+
+<html lang="en">
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>GameSpace</title>
 	<link href="https://fonts.googleapis.com/css?family=Nova+Mono" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<link rel="stylesheet" type="text/css" href="styles/main2.css">
 	<link rel="stylesheet" type="text/css" href="styles/snake.css">
-	<link rel="icon" href="img/gs.png" type="image/x-icon">
-	<script src="js/phaser.min.js"></script>
+	<link rel="icon" href="images/gs.png" type="image/x-icon">
 </head>
+
 <body>
 	<header>
-		<div id="banner">
+		
+		<div id="header_logo">
 			<a href="index.php" >
-				<img id="titleIcon" alt="titleIcon" src="./img/titleIcon.png">
-			</a> 
-				<img id="bannerBG" alt="bannerImg" src="./img/banner.png" >
+				<img id="logo" alt="GameSpace Logo" src="./images/headerLogo.png">
+			</a>
 		</div>
-
-		<div id="nav">
-			<ul class="navbar">
-				<li> <a href="index.php"> Shooters </a> </li>
-				<li> <a href="index.php"> Driving </a> </li>
-				<li> <a href="index.php"> Strategy </a> </li>
-				<li> <a href="index.php"> Multiplayer </a> </li>
-				<li> <a href="index.php"> Top Games </a> </li>
+		
+		<div id="header_top">
+			<div id="buttons">
+				<button id="login" class="account_button"> Log In </button>
+				<button id="addUser" class="account_button">New User</button>
+				<div id="user">
+					<?php
+						if (isset($_SESSION['user'])){ 
+							echo "Hello ".$_SESSION['user']."!";
+						}
+					?>
+				</div>
+			</div> 
+		</div>
+		
+		<nav>
+			<ul id="navbar">
+				<li> <a href="arcade.php"> Arcade </a> </li>
+				<li> <a href="strategy.php"> Strategy </a> </li>
+				<li> <a href="index.php"> Puzzle </a> </li>
+				<li> <a href="Multiplayer.php"> Multiplayer </a> </li>
+				<li> <a href="topGames.php"> Top Games </a> </li>
 			</ul>
-		</div>
-				
-		<div>
-			<a href="index.php"> Log In </a>
+		</nav>
+		
+		<div id="loginModal" class="modal">
+			<div class="loginContent">
+				<span class="close">x</span> 
+				<br>
+				<img alt="titleIcon" src="./images/titleIcon.png">
+				<h1>LOGIN</h1>
+				<form name="login" action="login.php" method="POST"> 
+				Username:<br>
+				<input type="text" name="userName"><br> 
+				Password:<br> 
+				<input type="password" name="password"><br> 
+				<br>
+				<input type="submit" value="Login"> 
+				</form>  
+					<?php
+						if (isset($_SESSION['errors'])){
+							echo '<style type="text/css">
+								#loginModal {
+									display: Block;
+								}
+								</style>';
+							echo "Username or Password is Incorrect";
+							unset($_SESSION['errors']);
+						}
+					?>
+			</div>
+		</div> 
+		
+		<div id="addModal" class="modal">
+			<div class="addContent">
+				<span class="close">x</span> 
+				<br>
+				<img alt="titleIcon" src="./images/titleIcon.png">
+				<h1>New User</h1>
+				<form name="addUser" action="add_user.php" method="POST"> 
+				Username:<br>
+				<input type="text" name="userName"><br> 
+				Password:<br> 
+				<input type="password" name="password"><br> 
+				<br>
+				<input type="submit" value="Submit"> 
+				</form>
+					<?php
+						if (isset($_SESSION['errors'])){
+							echo '<style type="text/css">
+								#addModal {
+									display: Block;
+								}
+								</style>';
+							echo "Name not available.";
+							unset($_SESSION['errors']);
+						}
+					?>
+			</div>
 		</div>
 	</header>
 
-	<div id="left_menu">
-		<figure>
-			<a href="snake.php">
-				<img src="img/blockSnake.png" alt="BlockSnake">
-			</a>
-			<figCaption>
-				<a href="snake.php">
-					 Block Snake - by Real Games 
-				</a>
-			</figCaption>
-		</figure>
-		<br>
-		<figure>
-			<a href="splash.php">
-				<img src="img/cuber.jpg" alt="Splash">
-			</a>
-			<figCaption>
-				<a href="splash.php">
-					 Splash - by PuzzleWorld 
-				</a>
-			</figCaption>
-		</figure>
-	</div>
+	<div id="main_content">
 
-	<div id="game_container">
-		<div id="my_score">
-		Current Score: 0
+		<div id="game_container">
+			<h1>Splash</h1>
+			<div id="game_window">
+			</div>
+			<div id="my_score">
+			Current Score: 0
+			</div>
+			<div id="social">
+				Share your score
+				<a href="http://www.facebook.com" target="_blank">
+					<img src="images/facebook.png" alt="facebook">
+				</a>
+				<a href="http://twitter.com" target="_blank">
+					<img src="images/twitter.png" alt="twitter">
+				</a>
+				<a href="http://plus.google.com/" target="_blank">
+					<img src="images/googleplus.png" alt="google plus">
+				</a>
+			</div>
 		</div>
-		<div id="game_window">
-		</div>
-		<div class="social">
-			Share your score
-			<img src="img/fb.png" alt="facebook">
-			<img src="img/twitter.png" alt="twitter">
-		</div>
-	</div>
 
-	<div class="scores">
-		<strong> High Scores </strong>
-		<ul id="high_scores">
-			<li> Alex: 1050 </li>
-			<li> Sarah: 850 </li>
-		</ul>
-	</div>
-	
-		
-	<div class="comment_section">
-		<div id="comments">
-			<ul>
-				<li> a </li>
-				<li> b </li>
+		<div id="scores">
+			<h2> High Scores </h2>
+			<ul id="high_scores">
+				<li><strong>The Triumphant Top 20:</strong></li>
+				<li> 1: Alex - 1050 </li>
+				<li> 2:   evilmorty - 750 </li>
+				<li> 3: joe - 600 </li>
+				<li> 4:   rickjames - 100 </li>
+				<li> 5:   Unclaimed - 000 </li>
+				<li> 6:   Unclaimed - 000 </li>
+				<li> 7:   Unclaimed - 000 </li>
+				<li> 8:   Unclaimed - 000 </li>
+				<li> 9:   Unclaimed - 000 </li>
+				<li> 9:   Unclaimed - 000 </li>
+				<li> 10: Unclaimed - 000 </li>
+				<li> 11: Unclaimed - 000 </li>
+				<li> 12: Unclaimed - 000 </li>
+				<li> 13: Unclaimed - 000 </li>
+				<li> 14: Unclaimed - 000 </li>
+				<li> 15: Unclaimed - 000 </li>
+				<li> 16: Unclaimed - 000 </li>
+				<li> 17: Unclaimed - 000 </li>
+				<li> 18: Unclaimed - 000 </li>
+				<li> 19: Unclaimed - 000 </li>
+				<li> 20: Unclaimed - 000 </li>
+				
 			</ul>
 		</div>
-		<form action="index.php" method="post">
-		<input type="text" name="comment" placeholder="Leave a comment" style="width: 85%;"/>
-		<input type="submit"/>
-		</form>
+		
+		<div id="more_games">
+			
+			<figure>
+				<a href="snake.php">
+					<img src="images/blockSnake.png" alt="BlockSnake">
+				</a>
+				<figCaption>
+					<a href="snake.php">
+						 Block Snake
+					</a>
+				</figCaption>
+			</figure>
+			
+			<figure>
+				<a href="splash.php">
+					<img src="images/cuber.jpg" alt="Splash">
+				</a>
+				<figCaption>
+					<a href="splash.php">
+						 Splash 
+					</a>
+				</figCaption>
+			</figure>
+			
+			<figure>
+				<a href="snake.php">
+					<img src="images/blockSnake.png" alt="BlockSnake">
+				</a>
+				<figCaption>
+					<a href="snake.php">
+						 Block Snake
+					</a>
+				</figCaption>
+			</figure>
+			
+			<figure>
+				<a href="splash.php">
+					<img src="images/cuber.jpg" alt="Splash">
+				</a>
+				<figCaption>
+					<a href="splash.php">
+						 Splash 
+					</a>
+				</figCaption>
+			</figure>
+			
+			<figure>
+				<a href="snake.php">
+					<img src="images/blockSnake.png" alt="BlockSnake">
+				</a>
+				<figCaption>
+					<a href="snake.php">
+						 Block Snake
+					</a>
+				</figCaption>
+			</figure>
+			
+			<figure>
+				<a href="splash.php">
+					<img src="images/cuber.jpg" alt="Splash">
+				</a>
+				<figCaption>
+					<a href="splash.php">
+						 Splash 
+					</a>
+				</figCaption>
+			</figure>
+			
+		</div>
 	</div>
+	
+	<div id="comment_wrapper">
+		<div id="comment_section">
+			
+			<div id="comForm">
+				<form id="commentForm" action="addComments.php" method="POST">
+					<textarea id="commentTextbox" name="comment" form="commentForm" onclick = "eraseText()">Enter comment here...</textarea>
+					<input type="submit" value="Post">
+				</form>  
+			</div>
+			
+			<div id="comments">
+				<table>
+					<thead>
+						<tr>
+							<td style="width:60px">Time</td>
+							<td style="width:120px">By</td>
+							<td>Comment</td>
+						</tr>
+					</thead>
+					<tbody id="commentBox">
 
-
-<script src="js/splash.js"></script>
+					</tbody> 
+				</table>
+			</div>
+			
+		</div>
+	</div>
+	
+	<footer>
+			<a href="index.php">Home</a> | <a href="index.php">Arcade</a> | <a href="index.php">Strategy</a> | <a href="index.php">Puzzle</a> | <a href="index.php">Multiplayer</a><br/><br/>
+			<a href="index.php">Top Games</a>  | website by <a href="index.php">GameSpace.inc</a>
+	</footer>
+	
+	<script src="js/splash.js"></script>
+	<script src="js/jquery-1.12.3.min.js"></script> 
+	<script src="js/comments.js"></script>
+	<script src="js/login.js"></script>
 </body>
+
 </html>
