50dac9f2b66af59871980bb4832490ec8ea714d8
Elizabeth Walker <eliwalks84@gmail.com>
2016-05-28 23:00:42 +1000

    Add high score

--- addHighScore.php
+++ addHighScore.php
@@ -1,5 +1,22 @@
 <?php
 session_start();
+	include 'database_info.php';
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	
+	$stmt = $conn->prepare("INSERT INTO highscores(highScore, userName, game) VALUES(?, ?, ?)");
+	$stmt->bind_param("iss", $highScore, $userName, $game);
+
+	$highScore = $_SESSION['score'];
+	$userName = $_SESSION['user'];
+	$game = $_SESSION['game'];
+	
+	$stmt->execute();
+
+	$stmt->close();
+	mysqli_close($conn);
+	
 	
 
 ?>
